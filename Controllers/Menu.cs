6aecbfe9ba44589ce52376865853d8c87d2183e6
robert <kasza.robert@stud.uni-obuda.hu>
2026-03-24 11:53:22 +0100

    Menu Controller Added

---
+++ Controllers/Menu.cs
@@ -0,0 +1,33 @@
+﻿using Menu.Data;
+using Menu.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Menu.Controllers
+{
+    public class Menu : Controller
+    {
+        private readonly MenuContext _context;
+        public Menu(MenuContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View( await _context.Dishes.ToListAsync());
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            var Dish = await _context.Dishes
+                .Include(di => di.Dishingredients)
+                .ThenInclude(i => i.Ingredient)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (Dish == null) 
+            {
+                return NotFound();
+            }
+            return View(Dish);
+        }
+    }
+}
